bf8dc9cd3c1f770b23d00d04137c0fda88266681
Alix Tiran-Cappello <alix.tiran-cappello@laposte.net>
2021-08-17 15:15:38 +0200

    Added model performance tracking MLFlow

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -9,6 +9,11 @@ from TaxiFareModel.data import get_data, clean_data, AWS_BUCKET_PATH
 from TaxiFareModel.encoders import TimeFeaturesEncoder, DistanceTransformer
 from TaxiFareModel.utils import compute_rmse
 
+from memoized_property import memoized_property
+import mlflow
+from  mlflow.tracking import MlflowClient
+
+
 class Trainer():
     def __init__(self, X, y):
         """
@@ -18,6 +23,8 @@ class Trainer():
         self.pipeline = None
         self.X = X
         self.y = y
+        self.MLFLOW_URI = "https://mlflow.lewagon.co/"
+        self.experiment_name = '[FR][Marseille][alixtc] TaxiFareModel - 1.0'
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
@@ -58,7 +65,35 @@ class Trainer():
         y_pred = self.pipeline.predict(X_test)
         rmse = compute_rmse(y_pred, y_test)
         print(rmse)
+        
+        self.mlflow_log_metric("rmse", rmse)
+        self.mlflow_log_param("model", self.pipeline[-1])
         return rmse
+    
+    # ------------- ML Flow --------------
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(self.MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+
+
 
 if __name__ == "__main__":    
     # store the data in a DataFrame
