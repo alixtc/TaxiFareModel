53955d18e3dd6298529518cc30a7e01a2e85227b
Alix Tiran-Cappello <alix.tiran-cappello@laposte.net>
2021-08-17 12:39:12 +0200

    Trainer: add pipeline building + fit predict meths

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -1,4 +1,12 @@
 # imports
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from TaxiFareModel.encoders import TimeFeaturesEncoder, DistanceTransformer
+
+def compute_rmse(y_pred, y_true):
+    return np.sqrt(((y_pred - y_true)**2).mean())
 
 class Trainer():
     def __init__(self, X, y):
@@ -12,16 +20,44 @@ class Trainer():
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
+        
+        # Pipelines to process Distance and Time columns
+        dist_pipe = Pipeline([
+            ('dist_trans', DistanceTransformer()),
+            ('stdscaler', StandardScaler())
+        ])
+        time_pipe = Pipeline([
+            ('time_enc', TimeFeaturesEncoder('pickup_datetime')),
+            ('ohe', OneHotEncoder(handle_unknown='ignore'))
+        ])
+
+        # combine two previous pipes on specified columns
+        preproc_pipe = ColumnTransformer([
+            ('distance', dist_pipe, ["pickup_latitude", "pickup_longitude", 'dropoff_latitude', 'dropoff_longitude']),
+            ('time', time_pipe, ['pickup_datetime'])
+        ], remainder="drop")
         pass
+    
+        # Add the model of your choice to the pipeline
+        pipe = Pipeline([
+            ('preproc', preproc_pipe),
+            ('linear_model', LinearRegression())
+        ])
+        self.pipeline = pipe
+        return self
+
 
     def run(self):
         """set and train the pipeline"""
-        pass
+        self.pipeline.fit(self.X, self.y)
+        return self
 
     def evaluate(self, X_test, y_test):
         """evaluates the pipeline on df_test and return the RMSE"""
-        pass
-
+        y_pred = self.pipeline.predict(X_test)
+        rmse = compute_rmse(y_pred, y_test)
+        print(rmse)
+        return rmse
 
 if __name__ == "__main__":
     # get data
